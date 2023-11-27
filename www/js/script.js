8cde2ba2cdfa0e4ef6de9934292d348707907135
josephbessa <117731244+josephbessa@users.noreply.github.com>
2023-11-27 11:14:59 -0300

    script.js

--- www/js/script.js
+++ www/js/script.js
@@ -1,4 +1,10 @@
-    // Adiciona o comportamento de rolar suavemente ao clicar nos links da barra de navegação
+// Função para rolar até o topo da página
+function topFunction() {
+    document.body.scrollTop = 0; // Para navegadores da web
+    document.documentElement.scrollTop = 0; // Para IE/Edge
+}
+
+// Adiciona o comportamento de rolar suavemente ao clicar nos links da barra de navegação
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
@@ -16,4 +22,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-
